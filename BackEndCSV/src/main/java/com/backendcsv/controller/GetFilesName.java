a6f9b8f0d2cc750730e5f053e8988ebb2ecdba9c
1mpleX <daniladubinkin@gmail.com>
2024-10-26 23:14:01 +0400

    add search files

---
+++ BackEndCSV/src/main/java/com/backendcsv/controller/GetFilesName.java
@@ -0,0 +1,36 @@
+package com.backendcsv.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class GetFilesName {
+
+    private final String directoryPath = "uploads"; // Замените на путь к вашей директории
+
+    @GetMapping("/MyFiles")
+    public List<String> listFiles() {
+        List<String> fileNames = new ArrayList<>();
+        File directory = new File(directoryPath);
+
+        if (directory.isDirectory()) {
+
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+
+                    if (file.isFile()) {
+                        fileNames.add(file.getName());
+                    }
+                }
+            }
+        }
+
+        return fileNames;
+    }
+
+}
